65e212694ce0d74fb89b5921ce639f9ee6710b39
Justin Ochoa <just.och92@gmail.com>
2020-07-18 18:42:42 -0400

    inital html & Css setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputs = document.getElementsByTagName("input");
+console.log(inputs);
+
+const header = document.getElementsByClassName("special");
+console.log(header);
+
+const veggies = document.querySelector("body section ul li.special");
+console.log(veggies);
